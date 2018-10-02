2d8ff556ec4c5d0b6439064f28d78f1208639a1a
Matt Outen <mattoutenphysed@gmail.com>
2018-10-01 19:23:32 -0500

    fix checkoint-6

--- src/policies/application.js
+++ src/policies/application.js
@@ -1,47 +1,43 @@
 module.exports = class ApplicationPolicy {
 
-    constructor(user, record) {
-      this.user = user;
-      this.record = record;
-    }
-  
-    // _isStandard() {
-    //   return this.record && this.user.role == "standard";
-    // }
-  
-    _isOwner() {
-      return this.record && (this.record.userId == this.user.id);
-    }
-  
-    _isAdmin() {
-      return this.user && this.user.role == "admin";
-    }
-  
-    // _isPremium() {
-    //   return this.user && this.user.role == "premium";
-    // }
-  
-    new() {
-      return this.user != null;
-    }
-  
-    create() {
-      return this.new();
-    }
-  
-    show() {
-      return true;
-    }
-  
-    edit() {
-      return this.new() && this.record && (this._isOwner() || this._isAdmin());
-    }
-  
-    update() {
-      return this.edit();
-    }
-  
-    destroy() {
-      return this.update();
-    }
-  };
+  constructor(user, record) {
+    this.user = user;
+    this.record = record;
+
+    console.log(user);
+    //The record part is showing up undefined, even with admin user
+    console.log(record);
+  }
+
+  _isOwner() {
+    return this.record && (this.record.userId == this.user.id);
+  }
+
+  _isAdmin() {
+    return this.user && this.user.role == "admin";
+  }
+
+  new() {
+    return this.user != null;
+  }
+
+  create() {
+    return this.new();
+  }
+
+  show() {
+    return true;
+  }
+
+  edit() {
+    return this.new();
+  }
+
+  update() {
+    return this.edit();
+  }
+
+  destroy() {
+    return this.update();
+  }
+}
